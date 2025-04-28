1da6f523b92415aec9215e33129851ddfd0da75a
FHobbs8030 <FHobbs8030@Gmail.com>
2025-04-28 13:48:37 -0700

    modify,delete,new files

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header"; // <-- no .jsx needed
+import Main from "./Main";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <p>This is your first Vite project</p>
+    </div>
+  );
+}
+
+export default Main;
