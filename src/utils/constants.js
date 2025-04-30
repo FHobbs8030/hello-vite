73b85c9bc597b51807ca66d69c40f9e8a4ede93c
FHobbs8030 <FHobbs8030@Gmail.com>
2025-04-29 21:56:46 -0700

    new files

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const data = [
+  { 
+    name: "cat",
+    image: new URL("../assets/animals/kitten1.jpg", import.meta.url).href
+  },
+  { 
+    name: "dog",
+    image: new URL("../assets/animals/pup1.jpg", import.meta.url).href
+  },
+  // etc...
