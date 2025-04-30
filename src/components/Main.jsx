9186764f054fbf75704319ba838194a690729b57
FHobbs8030 <FHobbs8030@Gmail.com>
2025-04-29 21:13:43 -0700

    modify and add fonts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Main.css";
 
 function Main() {
   return (
-    <div>
+    <div className="container">
       <p>This is your first Vite project</p>
+      <p>"Vite" means "quick" in French</p>
     </div>
   );
 }
