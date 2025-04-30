5bd29d370bbd39d7c13dea38b399fbad1efe6c99
FHobbs8030 <FHobbs8030@Gmail.com>
2025-04-29 22:14:47 -0700

    rename and modify

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,13 +1,10 @@
 import { data } from "../utils/constants.js";
 
-// selectedAnimal is a string, such as "cat", "dog", etc.
 function Animal({ selectedAnimal }) {
-  // Find the first animal that matches selectedAnimal.
   const animal = data.find((item) => {
     return item.name == selectedAnimal;
   });
 
-  // Use the selected animal's image and name in the JSX.
   return <img src={animal.image} alt={animal.name} />;
 }
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header"; 
 import Main from "./Main";
 import "./App.css";
+import Animal from "./Animal"
 
 function App() {
   return (
