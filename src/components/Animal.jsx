34e9c40842ab7689d0ab28c4548b40f50ea9d4b3
FHobbs8030 <FHobbs8030@Gmail.com>
2025-04-30 14:45:29 -0700

    modify

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,18 +1,20 @@
 import { data, defaultAnimalImage } from "../utils/constants";
 import "./Animal.css";
 
-function Animal({ selectedAnimal }) {
-  const animal = data.find((item) => item.name === selectedAnimal);
+const animal = data.find((item) => item.name === selectedAnimal);
 
-  const imageSrc = animal ? animal.image : defaultAnimalImage;
-  const name = animal ? animal.name : "Unknown animal";
+const imageSrc = animal ? animal.image : defaultAnimalImage;
+const name = animal ? animal.name : "Unknown animal";
 
-  return (
+return (
+  <div className="container">
     <div className="text-row">
       <p className="left-text">This is your first Vite project</p>
       <p className="right-text">"Vite" means "quick" in French.</p>
     </div>
-  );
-}
+    <img className="animal-image" src={imageSrc} alt={name} />
+  </div>
+);
+
 
 export default Animal;
