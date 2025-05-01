0ba5657b6164a585faef564c6415cd9d2eea320b
FHobbs8030 <FHobbs8030@Gmail.com>
2025-05-01 10:27:33 -0700

    modify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
-import Animal from "./Animal"; 
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Main />
-      <Animal selectedAnimal="dog" /> 
     </div>
   );
 }
